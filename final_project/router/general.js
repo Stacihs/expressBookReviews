c26d710623fddcd44900c262bfd8b40027cd352e
Stacihs <staci.holmessegarra>
2024-03-26 23:09:49 -0400

    Add search by author functionality

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+let booksArr = Object.entries(books);
 
 
 public_users.post("/register", (req, res) => {
@@ -18,17 +19,19 @@ public_users.get('/', function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-    let booksArr = Object.entries(books);
     let isbn = parseInt(req.params.isbn);
-    
-    res.send(booksArr[isbn-1]);
+    res.send(books[isbn]);
     return res.status(300).json({ message: "Yet to be implemented" });
-    
+
 });
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-    //Write your code here
+    let author = req.params.author;
+    const filteredByAuthor = booksArr.filter((book) => book.author === author);      
+    res.send(filteredByAuthor);
+    
+
     return res.status(300).json({ message: "Yet to be implemented" });
 });
 
