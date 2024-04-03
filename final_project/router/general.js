d92e3d4fcc205783e4758fa397a32a2c7fb43237
Stacihs <staci.holmessegarra@gmail>
2024-04-03 18:14:08 -0400

    Add promise callbacks to router/general.js for getting all books and getting book by ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+let prompt = require('prompt-sync')();
 
 
 public_users.post("/register", (req, res) => {
@@ -29,6 +30,23 @@ public_users.get('/', function (req, res) {
     return res.status(200).json({ message: "Success" });
 });
 
+// Promise callback to the console
+let getBooks = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        try {
+            const data = JSON.stringify(books, null, 4);
+            resolve(data);
+        } catch (err) {
+            reject(err);
+        }
+    }, 6000)
+})
+
+getBooks.then(
+    (data) => console.log(data),
+    (err) => console.log("Error getting books")
+    );
+
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
@@ -36,6 +54,25 @@ public_users.get('/isbn/:isbn', function (req, res) {
     res.send(books[isbn]);
     return res.status(200).json({ message: "Success" });
 });
+
+let getBookByISBN = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        try {
+            let isbn = parseInt(prompt("What is the book number?"));
+            resolve(books[isbn]);
+        } catch (err) {
+            reject(err);
+        }
+    }, 6000)
+})
+
+getBookByISBN.then(
+    (isbn) => console.log(books[isbn]),
+    (err) => console.log("Error getting book")
+    );
+
+
+
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
