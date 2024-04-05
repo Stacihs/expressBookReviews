1bb3bb9319a3065e4df7cae2a25c690f6da27ccd
Stacihs <staci.holmessegarra@gmail.com>
2024-04-04 23:55:27 -0400

    Modify get methods for books, in router/general.js, by using Promise callbacks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,7 +3,6 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
-let prompt = require('prompt-sync')();
 
 
 public_users.post("/register", (req, res) => {
@@ -26,85 +25,85 @@ public_users.post("/register", (req, res) => {
 
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
-    res.send(JSON.stringify(books, null, 4));
-    return res.status(200).json({ message: "Success" });
+    new Promise((resolve, reject) => {
+        let allBooks = JSON.stringify(books, null, 4);
+        resolve(allBooks);
+    })
+        .then(allBooks => {
+            return res.status(200).json({ message: "Success", allBooks });
+        })
+        .catch(err => {
+            return res.status(500).json({ message: "Error getting books" });
+        })
+
 });
-
-// Promise callback to the console
-let getBooks = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        try {
-            const data = JSON.stringify(books, null, 4);
-            resolve(data);
-        } catch (err) {
-            reject(err);
-        }
-    }, 6000)
-})
-
-getBooks.then(
-    (data) => console.log(data),
-    (err) => console.log("Error getting books")
-    );
 
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
     let isbn = parseInt(req.params.isbn);
-    res.send(books[isbn]);
-    return res.status(200).json({ message: "Success" });
+    new Promise((resolve, reject) => {
+        let book = books[isbn];
+        resolve(book);
+    })
+        .then(book => {
+            return res.status(200).json({ message: "Success", book });
+        })
+        .catch(err => {
+            console.log("Error:", err);
+            res.status(400).json({ message: "Book not found" });
+        })
 });
-
-let getBookByISBN = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        try {
-            let isbn = parseInt(prompt("What is the book number?"));
-            resolve(books[isbn]);
-        } catch (err) {
-            reject(err);
-        }
-    }, 6000)
-})
-
-getBookByISBN.then(
-    (isbn) => console.log(books[isbn]),
-    (err) => console.log("Error getting book")
-    );
-
 
 
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
     const author = req.params.author;
-    let filtered_authors = [];
-    for (let bookId in books) {
-        if (books.hasOwnProperty(bookId)) {
-            let book = books[bookId];
-            if (book.author === author) {
-                filtered_authors.push(book);
+    new Promise((resolve, reject) => {
+        let filtered_authors = [];
+        for (let bookId in books) {
+            if (books.hasOwnProperty(bookId)) {
+                let book = books[bookId];
+                if (book.author === author) {
+                    filtered_authors.push(book);
+                }
             }
         }
-    }
-    res.send(filtered_authors);
-    return res.status(200).json({ message: "Success" });
+        resolve(filtered_authors);
+    })
+        .then(filtered_authors => {
+            return res.status(200).json({ message: "Success", filtered_authors });
+        })
+        .catch(err => {
+            console.log("Error", err);
+            return res.status(400).json({ message: "Author not found" });
+        })
 });
 
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
     const title = req.params.title;
-    let filtered_titles = [];
-    for (let bookId in books) {
-        if (books.hasOwnProperty(bookId)) {
-            let book = books[bookId];
-            if (book.title === title) {
-                filtered_titles.push(book);
+    new Promise((resolve, reject) => {
+        let filtered_titles = [];
+        for (let bookId in books) {
+            if (books.hasOwnProperty(bookId)) {
+                let book = books[bookId];
+                if (book.title === title) {
+                    filtered_titles.push(book);
+                }
             }
         }
-    }
-    res.send(filtered_titles);
-    return res.status(200).json({ message: "Success" });
+        resolve(filtered_titles);
+    })
+        .then(filtered_titles => {
+            return res.status(200).json({ message: "Success", filtered_titles });
+        })
+        .catch(err => {
+            console.log("Error", err);
+            return res.status(400).json({ message: "Title not found" });
+        })
 });
 
 
